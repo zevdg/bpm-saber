cb18d7099e2c2664e366f755c1f03c2e9eb37004
Zev Goldstein <zev.goldstein@gmail.com>
2018-07-03 21:40:24 -0400

    work around lack of folder selection

--- main.go
+++ main.go
@@ -33,27 +33,51 @@ func run() error {
 	err := ui.Main(func() {
 		window := ui.NewWindow("MeterSaber", 1200, 100, false)
 
-		inputFolderEntry := ui.NewEntry()
-		inputFolderEntry.SetText(cliInputs.inputFolder)
-		inputFolderButton := ui.NewButton("Browse")
-		inputFolderButton.OnClicked(func(btn *ui.Button) {
-			inputFolderEntry.SetText(ui.OpenFile(window))
-		})
+		inputSongInfoEntry := ui.NewEntry()
+		if cliInputs.inputFolder != "" {
+			inputSongInfoEntry.SetText(filepath.Join(cliInputs.inputFolder, "info.json"))
+		}
+
+		inputSongInfoButton := ui.NewButton("Browse")
+
 		outputFolderEntry := ui.NewEntry()
 		outputFolderEntry.SetText(cliInputs.outputFolder)
-		outputFolderButton := ui.NewButton("Browse")
-		outputFolderButton.OnClicked(func(btn *ui.Button) {
-			outputFolderEntry.SetText(ui.OpenFile(window))
+		canOverrideOutputFolder := true
+		outputFolderEntry.OnChanged(func(e *ui.Entry) {
+			canOverrideOutputFolder = e.Text() == ""
 		})
+		updateOutputFolder := func() {
+			if !canOverrideOutputFolder {
+				return
+			}
+			if inputSongInfoEntry.Text() == "" {
+				outputFolderEntry.SetText("")
+				return
+			}
+			outputFolderEntry.SetText(filepath.Join(filepath.Dir(filepath.Dir(inputSongInfoEntry.Text())), "OUTPUT_FOLDER_NAME"))
+		}
+		inputSongInfoButton.OnClicked(func(btn *ui.Button) {
+			inputSongInfoEntry.SetText(ui.OpenFile(window))
+			updateOutputFolder()
+		})
+		inputSongInfoEntry.OnChanged(func(e *ui.Entry) {
+			updateOutputFolder()
+		})
+
 		inputBpmEntry := ui.NewEntry()
 		if cliInputs.inputBPM != 0 {
 			inputBpmEntry.SetText(floatToString(cliInputs.inputBPM))
 		}
 		loadInputBpmButton := ui.NewButton("load from song info")
 		loadInputBpmButton.OnClicked(func(btn *ui.Button) {
-			bpm, err := loadBpmFromSongInfo(inputFolderEntry.Text())
+			if err := validateSongInfo(inputSongInfoEntry.Text()); err != nil {
+				ui.MsgBoxError(window, "error", err.Error())
+				return
+			}
+
+			bpm, err := loadBpmFromFolder(filepath.Dir(inputSongInfoEntry.Text()))
 			if err != nil {
-				ui.MsgBoxError(window, "error", "couldn't load bpm from song info in directory '"+inputFolderEntry.Text()+"': "+err.Error())
+				ui.MsgBoxError(window, "error", "couldn't load bpm from song info '"+inputSongInfoEntry.Text()+"': "+err.Error())
 				return
 			}
 			inputBpmEntry.SetText(floatToString(bpm))
@@ -67,9 +91,10 @@ func run() error {
 		if cliInputs.outputBPM != 0 {
 			outputBpmEntry.SetText(floatToString(cliInputs.outputBPM))
 		}
-		loadOutputBpmButton := ui.NewButton("load from song info")
+		loadOutputBpmButton := ui.NewButton("load from output folder")
 		loadOutputBpmButton.OnClicked(func(btn *ui.Button) {
-			bpm, err := loadBpmFromSongInfo(outputFolderEntry.Text())
+			validateOutputFolder(outputFolderEntry.Text())
+			bpm, err := loadBpmFromFolder(outputFolderEntry.Text())
 			if err != nil {
 				ui.MsgBoxError(window, "error", "couldn't load bpm from song info in directory '"+outputFolderEntry.Text()+"': "+err.Error())
 				return
@@ -92,17 +117,16 @@ func run() error {
 		box.SetPadded(true)
 		box.Append(ui.NewLabel("All fields are required"), false)
 
-		inputFolderBox := ui.NewHorizontalBox()
-		inputFolderBox.SetPadded(true)
-		inputFolderBox.Append(inputFolderButton, false)
-		inputFolderBox.Append(inputFolderEntry, true)
-		inputFolderGroup := ui.NewGroup("input folder")
-		inputFolderGroup.SetChild(inputFolderBox)
-		box.Append(inputFolderGroup, false)
+		inputSongInfoBox := ui.NewHorizontalBox()
+		inputSongInfoBox.SetPadded(true)
+		inputSongInfoBox.Append(inputSongInfoButton, false)
+		inputSongInfoBox.Append(inputSongInfoEntry, true)
+		inputSongInfoGroup := ui.NewGroup("input song info.json")
+		inputSongInfoGroup.SetChild(inputSongInfoBox)
+		box.Append(inputSongInfoGroup, false)
 
 		outputFolderBox := ui.NewHorizontalBox()
 		outputFolderBox.SetPadded(true)
-		outputFolderBox.Append(outputFolderButton, false)
 		outputFolderBox.Append(outputFolderEntry, true)
 		outputFolderGroup := ui.NewGroup("output folder")
 		outputFolderGroup.SetChild(outputFolderBox)
@@ -163,7 +187,7 @@ func run() error {
 		window.SetMargined(true)
 		window.SetChild(box)
 		button.OnClicked(func(*ui.Button) {
-			inputs, err := validateInputs(inputFolderEntry.Text(), outputFolderEntry.Text(), inputBpmEntry.Text(), outputBpmEntry.Text())
+			inputs, err := validateInputs(inputSongInfoEntry.Text(), outputFolderEntry.Text(), inputBpmEntry.Text(), outputBpmEntry.Text())
 			if err != nil {
 				ui.MsgBoxError(window, "invalid input", err.Error())
 				return
@@ -187,7 +211,7 @@ func run() error {
 	// return process(inputs)
 }
 
-func loadBpmFromSongInfo(songFolderPath string) (float64, error) {
+func loadBpmFromFolder(songFolderPath string) (float64, error) {
 	if err := ensureDir(songFolderPath); err != nil {
 		return 0, err
 	}
@@ -198,18 +222,35 @@ func loadBpmFromSongInfo(songFolderPath string) (float64, error) {
 	return info.BeatsPerMinute, nil
 }
 
-func validateInputs(inputFolder, outputFolder, inputBPM, outputBPM string) (*inputFields, error) {
-	in := &inputFields{}
-	if err := ensureDir(inputFolder); err != nil {
-		return nil, fmt.Errorf("input folder '%s': %s", inputFolder, err)
+func validateSongInfo(inputSongInfo string) error {
+	if err := ensureFile(inputSongInfo); err != nil {
+		return fmt.Errorf("input song info '%s': %s", inputSongInfo, err)
 	}
-	in.inputFolder = inputFolder
+	if filepath.Base(inputSongInfo) != "info.json" {
+		return fmt.Errorf("input song info '%s': file must be named info.json", inputSongInfo)
+	}
+	return nil
+}
+
+func validateOutputFolder(outputFolder string) error {
+	if err := ensureDir(outputFolder); err != nil {
+		return fmt.Errorf("output folder '%s': %s", outputFolder, err)
+	}
+	return nil
+}
+
+func validateInputs(inputSongInfo, outputFolder, inputBPM, outputBPM string) (*inputFields, error) {
+	in := &inputFields{}
+	if err := validateSongInfo(inputSongInfo); err != nil {
+		return nil, err
+	}
+	in.inputFolder = filepath.Dir(inputSongInfo)
 
 	if err := os.MkdirAll(outputFolder, 0755); err != nil {
 		return nil, fmt.Errorf("couldn't create output folder '%s': %s", outputFolder, err)
 	}
-	if err := ensureDir(outputFolder); err != nil {
-		return nil, fmt.Errorf("output folder '%s': %s", outputFolder, err)
+	if err := validateOutputFolder(outputFolder); err != nil {
+		return nil, err
 	}
 	in.outputFolder = outputFolder
 
@@ -248,6 +289,17 @@ func ensureDir(path string) error {
 	}
 	if !fi.IsDir() {
 		return errors.New("is a file, not a folder")
+	}
+	return nil
+}
+
+func ensureFile(path string) error {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return errors.New("no such file or directory")
+	}
+	if fi.IsDir() {
+		return errors.New("is a folder, not a file")
 	}
 	return nil
 }
